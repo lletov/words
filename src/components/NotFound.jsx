931a1ab740ce4764239e65d940422a72eab519b0
Dmitry <dmitry2595@gmail.com>
2024-10-18 15:29:37 +0300

    feat: add notfound page content

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import satellite from './../assets/satellite.png'
+import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='not-found'>
+      <img src={satellite}></img>
+      <h3>ой!</h3>
+      <p>похоже, такой страницы не существует или возникла ошибка</p>
+      <Link to='/'><button className='btn-s'>на главную страницу</button></Link>
+    </div>
   )
 }
