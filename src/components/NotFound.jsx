109fbb8aee0dd712e7e5d186a70ce2261a683e82
Dmitry <dmitry2595@gmail.com>
2024-10-22 12:46:29 +0300

    feat: 404 page new img

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import satellite from './../assets/satellite.png'
+import cat from './../assets/cat.png'
 import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
   return (
     <div className='not-found'>
-      <img src={satellite}></img>
+      <img src={cat}></img>
       <h3>Ой!</h3>
       <p>Похоже, такой страницы не существует или возникла ошибка</p>
       <Link to='/'><button className='btn-s'>на главную страницу</button></Link>
