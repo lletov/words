2d413e3bdbf6c6068d7790f2c1afd9f7307d9aae
Dmitry <dmitry2595@gmail.com>
2024-10-29 14:33:05 +0300

    feat: add weather fetch file

---
+++ src/components/Weather.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Weather = () => {
+  return (
+    <div className='content'>
+        <h5>Погода в Англии</h5>
+        <p>Посмотреть обстановку в разных городах</p>
+    </div>
+  )
+}
