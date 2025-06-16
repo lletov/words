2af3ec5e36e1a4394f6843b2dbb08b1c271a307e
Dmitry <dmitry2595@gmail.com>
2025-06-16 11:00:10 +0300

    feat: add new words file basic obj structure

---
+++ src/Words1.js
@@ -0,0 +1,12 @@
+const words = {
+    'apple': {
+        translation: ['яблоко'],
+        level: ['A1', 'A2'],
+        category: ['fruits', 'things']
+    },
+    'bag': {
+        translation: ['сумка'],
+        level: ['A1', 'A2'],
+        category: ['things']
+    },
+}
