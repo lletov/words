945c46d5b59a597466cbb1455996159bd29f134e
Dmitry <dmitry2595@gmail.com>
2026-05-09 11:47:52 +0300

    fix: use similar words

--- src/utils/TestSetupUtils.js
+++ src/utils/TestSetupUtils.js
@@ -1,41 +1,49 @@
-export function generareRandomList(arr, n){
-    // generate random list with n elements from input array arr, with no repeated elements
-    let result = [];
-    for (let i = 0; i < n; i++){
-      let word = arr[Math.floor(Math.random() * arr.length)];
-        while (result.includes(word)){
-          word = arr[Math.floor(Math.random() * arr.length)]
-        }
-      result[i] = word
+export function generareRandomList(arr, n) {
+  // generate random list with n elements from input array arr, with no repeated elements
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    let word = arr[Math.floor(Math.random() * arr.length)];
+    while (result.includes(word)) {
+      word = arr[Math.floor(Math.random() * arr.length)]
     }
-    return result
+    result[i] = word
   }
+  return result
+}
 
 export function shuffle(array) {
-    // shuffle inputed array
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+  // shuffle inputed array
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+import words from '../Words';
+export function generateTestObject(arr) {
+  // generate test object
+  let testArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let id = i;
+    const targetPartOfSpeech = words[arr[i]].category;
+    console.log(arr[i], targetPartOfSpeech)
+    const targetPartOfSpeechWordsArray =
+      Object.entries(words).filter(([key, value]) =>
+        value.category.some(partOfSpeech => targetPartOfSpeech.includes(partOfSpeech))
+      ).map(([key, value]) => key);
+    let refArr = targetPartOfSpeechWordsArray.filter((word) => word !== arr[i])
+    let additionalArr = generareRandomList(refArr, 3)
+    let variants = [arr[i]].concat(additionalArr);
+    shuffle(variants);
+    let correctIndex = variants.indexOf(arr[i])
+    let elem = {
+      id: id,
+      correctIndex: correctIndex,
+      variants: variants
     }
+    testArr.push(elem)
   }
 
-export function generateTestObject(arr){
-    // generate test object
-    let testArr = [];
-    for (let i = 0; i < arr.length; i++){
-      let id = i;
-      let refArr = arr.filter((word) => word !== arr[i])
-      let additionalArr = generareRandomList(refArr, 3)
-      let variants = [arr[i]].concat(additionalArr);
-      shuffle(variants);
-      let correctIndex = variants.indexOf(arr[i])
-      let elem = {
-        id: id,
-        correctIndex: correctIndex,
-        variants: variants
-      }
-      testArr.push(elem)
-    }
-    return testArr
-  }
+  return testArr
+}
 
